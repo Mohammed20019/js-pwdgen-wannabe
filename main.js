ecb0f062985706b8d653c1da1814a351bd124401
Mohammed jihad <mohammedjihad229@gmail.com>
2023-03-28 14:32:43 +0200

    aggiuto javascript

---
+++ main.js
@@ -0,0 +1,7 @@
+
+const nome = prompt('Qual è il tuo nome?');
+const cognonome = prompt('inserire cognome');
+let colorepreferito = prompt('colore preferito');
+let presentazione = ` ciao sono ${nome} ${cognonome} è 
+il mio colore preferito è ${colorepreferito} `;
+console.log(presentazione);
